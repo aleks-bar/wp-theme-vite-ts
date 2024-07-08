6728623e0c91e75c686dcc59dfa693715b30ac0f
AlexBar <nesferat@mail.ru>
2024-07-08 14:21:59 +0300

    small fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,32 @@ import { defineConfig, loadEnv } from 'vite';
 import { viteConfig } from './config/vite/viteConfig';
 import { ViteOptions } from './config/vite/types/vite';
 
+interface EnvFile {
+    HOST: string,
+    PORT: string,
+    MODE: 'development' | 'production',
+    BUILD_DIR: string,
+    PUBLIC_DIR: string,
+    MANIFEST: string,
+    PATH_TO_THEME: string,
+    JS_CHUNKS: string
+}
+
 export default defineConfig( ( configEnv ) => {
     const { mode } = configEnv;
     // Получаем переменные env
-    const env = loadEnv( configEnv.mode, process.cwd(), '' );
+    const env = loadEnv( configEnv.mode, process.cwd(), '' ) as EnvFile;
+
+    const jsChunks = env.JS_CHUNKS?.split( ',' ) ?? [];
 
     const options: ViteOptions = {
         configEnv,
         mode,
         isDev: mode === 'development',
-        port: +env.DEV_PORT || 3000,
-        host: env.DEV_HOST || 'localhost',
+        port: +env.PORT || 3000,
+        host: env.HOST || 'localhost',
         domain: 'example.loc',
-        jsFile: env.MAIN_JS_FILE,
+        jsChunks,
         paths: {
             root: resolve( __dirname ),
             build: resolve( __dirname, env.BUILD_DIR || 'dist' ),
